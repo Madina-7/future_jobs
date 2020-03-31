4ce9688ce57185abbacecf6c6e38b51537244800
Your Name <you@example.com>
2020-03-31 23:34:43 +0200

    start request pole emploi to get jobs

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const port = 3001
 const fs = require('fs')
 const path = require('path')
 const router = express.Router();
+const rp = require('request-promise'); //to do HTTP request async
 
 app.use(express.static(path.join(__dirname, '/FutureJobs')));
 
@@ -27,22 +28,48 @@ app.get('/jobs', (req, res) => {
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
-// app.get('/test', (req, res) => {
-//     let url = ' https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=partenaire';
- 
-// fetch('POST' +url, {
-//     method: 'POST',
-//     headers: {
-//       "Content-type": "application/x-www-form-urlencoded;",
-//     },
-//     body: "grant_type=client_credentials&client_id=PAR_futurejob_136378497779aeb3f34e8ff8ed5d74105c30b4e095f3f413d51b3fe808148bd7&client_secret=493cc9870379dbf1cc17d968876623d4646d98a2a926749c92ebb1deddb8b969&scope=application_PAR_futurejob_136378497779aeb3f34e8ff8ed5d74105c30b4e095f3f413d51b3fe808148bd7"
-//   })
-//   .then((res)=>res.json())
-//   .then(function (data) {
-//     console.log('Request succeeded with JSON response', data);
-//   })
-//   .catch(function (error) {
-//     console.log('Request failed', error);
-//   });
-//     res.json(jobs);
-// });
+var options = {
+  method: 'POST',
+  uri: 'https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=partenaire',
+  body: "grant_type=client_credentials&client_id=PAR_workit_7baf0f55844af373a7f4928e0959957a5de15448e4914f495caaf3f67d32865a&client_secret=dca0e77b24c717d9d772042280090e7aecef5bf6128e2a8410d0ad42b0a2f8f5&scope=application_PAR_workit_7baf0f55844af373a7f4928e0959957a5de15448e4914f495caaf3f67d32865a",
+  headers: {
+    "Content-type": "application/x-www-form-urlencoded;",
+    },
+};
+
+
+ // promise is async function
+rp(options)
+  .then((res)=>{
+    res_json = JSON.parse(res); // convert string to json
+    console.log(res_json.access_token);
+
+    var option2 = {
+      method: 'GET',
+      uri: 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/referentiel/metiers',
+      headers: {
+        'Accept': 'application/json',
+        "Authorization": 'Bearer '+ res_json.access_token,   
+      },
+      maxRedirects: 20,
+      json: true 
+    };   
+    
+    // send http get request 
+    // put it insude the "then" function to exec after we get authorized (token)
+    rp(option2)
+      .then((res2)=>{
+
+        console.log(res2)
+      })
+      .catch(function (error) {
+        // if there is an error on the GET...
+        console.log('API call failed', error);
+      });
+    
+  })
+  .catch(function (error) {
+    // if there is an error on the POST...
+    console.log('Request failed', error);
+  });
+
