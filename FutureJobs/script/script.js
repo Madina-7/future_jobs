efc43704157e6526b2920dff35a5941eedf96074
Edouard.Zemb <edouard.zemb@schaeffer-productique.com>
2020-03-31 09:58:20 +0200

    commit test avec ajout result_page+ script.js

---
+++ FutureJobs/script/script.js
@@ -0,0 +1,10 @@
+
+const btnSubmit = document.querySelector("#submit");
+/**
+ * Permet d'afficher la page de résultat
+ */
+function onSubmit() {
+    
+};
+
+btnSubmit.addEventListener("click", onSubmit())
