18629f59caf4dc26b044bbe741b1a24f8cfd8f57
Edouard.Zemb <edouard.zemb@schaeffer-productique.com>
2020-04-01 10:15:34 +0200

    première requête get avec access token généré avec postman

--- FutureJobs/script/script.js
+++ FutureJobs/script/script.js
@@ -1,24 +1,15 @@
+var myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+myHeaders.append("Authorization", "Bearer b2e96a82-62d3-4008-a323-97de72a40893");
+myHeaders.append("Cookie", "TS01600790=01b3abf0a2c1b23cfa4a386ef7156d5f2c353d7e7d6d6a88a26b06f9334dbc9e27e36193b09b4d2229e2e50e88d4cc884eaa8629c1");
 
-/**
- * Authentification pour récupérer un access token
- */
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
-fetch('fake.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    appendData(data);
-  })
-  .catch(function (err) {
-    console.log('error: ' + err);
-  });
-
-  function appendData(data) {
-    var mainContainer = document.getElementById("myData");
-    for (var i = 0; i < data.length; i++) {
-      var div = document.createElement("div");
-      div.innerHTML = 'Name: ' + data[i].jobtitle + ' ' + data[i].location;
-      mainContainer.appendChild(div);
-    }
-  }
+fetch("https://api.emploi-store.fr/partenaire/offresdemploi/v2/referentiel/metiers", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
