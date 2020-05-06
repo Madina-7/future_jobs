e8165b2c3ae1dc47b7fd3e8cdeea1992153c20cb
Edouard.Zemb <edouard.zemb@schaeffer-productique.com>
2020-05-06 11:23:03 +0200

    click event on search button

--- FutureJobs/script/script.js
+++ FutureJobs/script/script.js
@@ -1,15 +1,30 @@
 var myHeaders = new Headers();
 myHeaders.append("Accept", "application/json");
-myHeaders.append("Authorization", "Bearer b2e96a82-62d3-4008-a323-97de72a40893");
-myHeaders.append("Cookie", "TS01600790=01b3abf0a2c1b23cfa4a386ef7156d5f2c353d7e7d6d6a88a26b06f9334dbc9e27e36193b09b4d2229e2e50e88d4cc884eaa8629c1");
+myHeaders.append(
+  "Authorization",
+  "Bearer b2e96a82-62d3-4008-a323-97de72a40893"
+);
+myHeaders.append(
+  "Cookie",
+  "TS01600790=01b3abf0a2c1b23cfa4a386ef7156d5f2c353d7e7d6d6a88a26b06f9334dbc9e27e36193b09b4d2229e2e50e88d4cc884eaa8629c1"
+);
 
 var requestOptions = {
-  method: 'GET',
+  method: "GET",
   headers: myHeaders,
-  redirect: 'follow'
+  redirect: "follow",
 };
 
-fetch("https://api.emploi-store.fr/partenaire/offresdemploi/v2/referentiel/metiers", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+fetch(
+  "https://api.emploi-store.fr/partenaire/offresdemploi/v2/referentiel/metiers",
+  requestOptions
+)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
+
+const btnSubmit = document.getElementById("btnSubmit");
+
+btnSubmit.addEventListener("click", () => {
+  window.location.replace(document.location + "result_page.html");
+});
